8e9c0e9296c5aa0c3aff665097a7a952eae73754
Oğuzhan Cüceoğlu <oguzhanncuceoglu@gmail.com>
2026-05-17 12:49:41 +0300

    js7 ödev gönderildi

--- js/task-6.js
+++ js/task-6.js
@@ -3,16 +3,35 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
-
-const changeColorBtn = document.querySelector('.change-color');
-const colorSpan = document.querySelector('.color');
-const body = document.body;
-
-changeColorBtn.addEventListener('click', () => {
- 
-  const newColor = getRandomHexColor();
-
-  body.style.backgroundColor = newColor;
-
-  colorSpan.textContent = newColor;
-});
+const controlsDiv = document.querySelector('#controls');
+const inputNum = controlsDiv.querySelector('input');
+const createBtn = controlsDiv.querySelector('[data-create]');
+const destroyBtn = controlsDiv.querySelector('[data-destroy]');
+const boxesContainer = document.querySelector('#boxes');
+createBtn.addEventListener('click', onCreateBoxes);
+destroyBtn.addEventListener('click', destroyBoxes);
+function onCreateBoxes() {
+  const amount = Number(inputNum.value);
+  if (amount < 1 || amount > 100 || isNaN(amount)) {
+    return;
+  }
+  destroyBoxes();
+  createBoxes(amount);
+  inputNum.value = '';
+}
+function createBoxes(amount) {
+  const fragment = document.createDocumentFragment();
+  let initialSize = 30;
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    const currentSize = initialSize + i * 10;
+    box.style.width = `${currentSize}px`;
+    box.style.height = `${currentSize}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    fragment.appendChild(box);
+  }
+  boxesContainer.appendChild(fragment);
+}
+function destroyBoxes() {
+  boxesContainer.innerHTML = '';
+}
